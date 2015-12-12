5b60a9c47990e892395b16bfec766557f6eff063
maurbeck <maurbeck@onedegree.com>
2015-12-11 23:04:32 -0600

    added cfgtodict
    Implemented cfgtodict to specify an info.cfg and return that upon /,
    /info and /help.
    
    Needs nesting support in cfgtodict so that we can define a set of
    endpoints together and have it all machine readable.
    
    https://github.com/joelklabo/cfgtodict

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## basic-flask21
 
-Basic flask server with /info and blank /example endpoints. Includes a settings.py file to pull out non-sensitive configuration info.
+Basic flask server with /info and blank /example endpoints. Includes a settings.py file to pull out non-sensitive configuration info and an info.cfg to define the program and its endpoints to end-users.
--- server.py
+++ server.py
@@ -3,10 +3,12 @@ import json
 
 from flask import Flask
 from flask import request 
+from flask.ext.responses import json_response
 
 from two1.lib.wallet import Wallet
 from two1.lib.bitserv.flask import Payment
 
+from cfgtodict import ConfigToDictionary
 from settings import *
 
 app = Flask(__name__)
@@ -18,36 +20,8 @@ payment = Payment(app, wallet)
 @app.route('/help')
 def home():
     '''Document the API so that other services could consume automatically.'''
-    home_obj = [{"name": "basic template",
-                 "service_version": "1",
-                 "api_version": "1",
-                 "description": "Describe your service in a couple of sentences or less.",
-                 "endpoints" : [
-                                {"route": "/example",
-                                 "args": [{"name": "first_arg",
-                                           "description": "Describe the first argument to this endpoint."},
-                                           ],  
-                                 "per-req": PRICE,
-                                 "description": "Briefly describe this endpoint.",
-                                 "returns": [{"name": "first_return",
-                                              "description": "Describe the first piece of data your service returns."},
-                                            ],
-                                },
-                                {"route": "/info",
-                                 "args": None,
-                                 "per-req": 0,
-                                 "description": "This listing of endpoints provided by this server. "\
-                                    "Available at /info."
-                                }],
-                }
-               ]
-
-    body = json.dumps(home_obj, indent=2)
-
-    return (body, 200, {'Content-length': len(body),
-                        'Content-type': 'application/json',
-                       }
-           )
+    about = ConfigToDictionary('info.cfg').dict()
+    return json_response(about)
 
 @app.route('/example')
 @payment.required(PRICE)
